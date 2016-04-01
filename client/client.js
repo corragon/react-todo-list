92b39e4ad05e0cd7813294ece1da6ce7d9c7d1b1
Thomas Y <corragon@gmail.com>
2016-04-01 13:36:36 -0500

    Complete part 3

--- client/client.js
+++ client/client.js
@@ -1,1 +1,11 @@
+import React from 'react'  //var react = require('react')
+import { render } from 'react-dom'
+import App from '../components/App.js'
 
+
+render(
+	// define the encompassing component
+	// DOM element we want to mount at
+	<App/>,
+	document.getElementById('app')
+	);
--- components/App.js
+++ components/App.js
@@ -1,1 +1,13 @@
+import React, { Component } from 'react'
 
+//or
+//var App = React.createClass(...)
+class App extends Component {
+
+	render() {
+		return <div>This is definitely a react app now!</div>
+	}
+}
+
+
+export default App
